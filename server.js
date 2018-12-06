e8a9ff9dbb3e3a79a57d9014c72f3990654f23e0
Joyce Luu <atsukara@gmail.com>
2018-12-05 19:49:45 -0800

    adding express

---
+++ server.js
@@ -0,0 +1,37 @@
+
+var http = require('http');
+var fs = require('fs');
+const hostname = '0.0.0.0';
+var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+app.use(express.static('css'));
+
+var server = http.createServer(function (req, res)
+{
+	if (req.url === '/')
+	{
+		fs.readFile('j1a_interview.html', function(err, data)
+		{
+			res.writeHead(200, {'Content-Type': 'text/HTML'});
+			res.write(data);
+			res.end();
+		});
+	}
+	else if (req.url === '/css/j1A.css')
+	{
+		fs.readFile('j1A.css', function(err, data)
+	{
+		res.writeHead(200, {'Content-Type': 'text/CSS'});
+		res.write(data);
+		res.end();
+	});
+	}
+
+});
+
+server.listen(port, hostname, function()
+{
+	console.log('Server running at http://' + hostname + ':' + port + '/');
+});
