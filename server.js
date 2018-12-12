77fc7b6da3fe6ecd7d89aff2806bfb4cc0de856f
Joyce Luu <atsukara@gmail.com>
2018-12-11 21:50:36 -0800

    cleaning up & adding ejs

--- server.js
+++ server.js
@@ -1,36 +1,16 @@
-var http = require('http');
-var fs = require('fs');
 const hostname = '0.0.0.0';
 var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs')
 
-app.use('/static', express.static('static'));
+app.use(express.static('public'));
 
-var server = http.createServer(function (req, res)
-{
-	if (req.url === '/')
-	{
-		fs.readFile('j1a_interview.html', function(err, data)
-		{
-			res.writeHead(200, {'Content-Type': 'text/HTML'});
-			res.write(data);
-			res.end();
-		});
-	}
-	else if (req.url === '/static/css/j1A.css')
-	{
-		fs.readFile('j1A.css', function(err, data)
-	{
-		res.writeHead(200, {'Content-Type': 'text/CSS'});
-		res.write(data);
-		res.end();
-	});
-	}
-
+app.get('/', (req, res) => {
+	res.render('j1a_interview')
 });
 
-server.listen(port, hostname, function()
-{
-	console.log('Server running at http://' + hostname + ':' + port + '/');
+
+app.listen(port, () => {
+	console.log('Listening on... ' + port);
 });
