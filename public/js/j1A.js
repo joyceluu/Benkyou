4f5a2eae78a24676e3b0214e14a02e2682ddde7b
Joyce Luu <atsukara@gmail.com>
2019-01-12 14:10:33 -0800

    adding in questions script

--- public/js/j1A.js
+++ public/js/j1A.js
@@ -1,9 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() { 
 
-$(document).ready(function()
-{
-	$.get('questions.txt', function(data)
-	{
-		var lines = data.split('\n');
-		$('.question').append(lines[0]);
-	});
+	// URL to questions list
+	const questionList = 'https://raw.githubusercontent.com/joyceluu/Benkyou/master/public/js/questions.txt';
+	const questions = []; // empty array
+	const question = document.querySelector('.question'); // grab question div
+	const button = document.querySelector('button'); // grab button
+
+	// grab text file contents, split by line and push to array
+	fetch(questionList)
+		.then(function(text)
+		{
+			text.text().then(function(text) {
+				text = text.split('\n');
+				questions.push(...text);
+			});
+		});
+
+	let index = 0; // to keep track of which question is being displayed
+
+
+	function displayQuestion() {
+		const html = questions[index];
+		if (index < questions.length - 1) {
+			index++;
+		}
+		else
+		{
+			index = 0;
+		}
+		console.log(index);
+		question.innerHTML = html;
+	}
+
+	button.addEventListener('click', displayQuestion);
 });
